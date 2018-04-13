55c82d817258c5c83f2308095ade0945b6bd897c
Quanjiang Lee <QuanjiangLee@gmail.com>
2018-04-13 21:30:15 +0800

    first commit

---
+++ VMBR_APP/__init__.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+from config import basedir
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)    #app instance
+app.config.from_object('config') #app configuration file
+db = SQLAlchemy(app)
+
+from VMBR_APP import views, models
